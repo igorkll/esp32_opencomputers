09bbc1760477ea6865f233c9dc6b37fed95ff2b8
igorkll <frerther@gmail.com>
2025-05-01 18:02:40 +0300

    Update eeprom.lua

--- esp32_opencomputers/rom/eeprom.lua
+++ esp32_opencomputers/rom/eeprom.lua
@@ -441,7 +441,7 @@ local function gui_menu_time()
         end
 
         if fill then
-            gpu.fill(pos - 1, numberLine, #image_num_0[1], #image_num_0, backgroundColor)
+            gpu.fill(pos - 1, numberLine, #image_num_0[1], #image_num_0, " ")
         end
         drawImage(pos - 1, numberLine, images_number[number], color)
     end
